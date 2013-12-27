e0d58832c9f10b98dcc566686d7e3a840d780c9d
Davenge <korisd@gmail.com>
2013-12-27 15:28:48 -0500

    added the strings_table.c

---
+++ strings_table.c
@@ -0,0 +1,9 @@
+/* File - strings_table.c
+ * Strings table, this is where you can find tables full of strings for things and stuff...
+ * Written by Davenge */
+
+#include "mud.h"
+
+const char *const nanny_strings[MAX_NANNY_TYPE][] = {
+   { "will this compile?" , "I don't know.", "Don't see why not?" }
+};
