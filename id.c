5ce8b08f2962f70d95932e3f5c1496a5b0621513
Davenge <korisd@gmail.com>
2014-01-15 23:08:40 -0500

    added and headed id.c and id.h

---
+++ id.c
@@ -0,0 +1,4 @@
+/* File id.c
+   All methods pertaining to the id.c go here
+   Written by Davenge */
+
---
+++ id.h
@@ -0,0 +1,5 @@
+/* File id.h
+   This is the header file for id.c
+   Written by Davenge */
+
+
